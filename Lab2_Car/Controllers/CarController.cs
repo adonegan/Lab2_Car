d2ba5df223ecd13a5db03a8d4a597686a50ec5a8
adonegan <donegan.aileen@gmail.com>
2024-03-20 23:21:13 +0000

    remove unused template controllers & replace Index() with Display()

--- Lab2_Car/Controllers/CarController.cs
+++ Lab2_Car/Controllers/CarController.cs
@@ -1,83 +1,18 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Lab2_Car.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Lab2_Car.Controllers
 {
     public class CarController : Controller
     {
-        // GET: CarController
-        public ActionResult Index()
+       
+        public ActionResult Display()
         {
-            return View();
+            Car car = new Car() { Make="Nissan", Model="March", Type=EngineType.Petrol, EngineSize="50CC"};
+            return View(car);
         }
 
-        // GET: CarController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: CarController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CarController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: CarController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: CarController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: CarController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: CarController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
+       
     }
 }

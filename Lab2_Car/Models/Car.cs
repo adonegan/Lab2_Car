b0cead44731fb6dba6af6d6f2aff650863fb3f1f
adonegan <donegan.aileen@gmail.com>
2024-03-20 23:24:20 +0000

    add model (before adding controller

--- Lab2_Car/Models/Car.cs
+++ Lab2_Car/Models/Car.cs
@@ -1,6 +1,6 @@
 ﻿namespace Lab2_Car.Models
 {
-    public enum EngineTypes
+    public enum EngineType
     {
         Petrol, Diesel, Hybrid
     }
@@ -9,7 +9,7 @@
     {
         public string Make { get; set; }
         public string Model { get; set; }
-        public EngineTypes Type { get; set; }
+        public EngineType Type { get; set; }
         public string EngineSize { get; set; }
     }
 }

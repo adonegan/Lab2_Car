2fe653f653166ac3fa2d35d3a1ebc635f41383f8
adonegan <donegan.aileen@gmail.com>
2024-03-20 23:10:58 +0000

    create model

---
+++ Lab2_Car/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿namespace Lab2_Car.Models
+{
+    public enum EngineTypes
+    {
+        Petrol, Diesel, Hybrid
+    }
+
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public EngineTypes Type { get; set; }
+        public string EngineSize { get; set; }
+    }
+}

ab757414f29443b6968880daf40bc4b1276cc336
adonegan <donegan.aileen@gmail.com>
2024-03-20 23:32:52 +0000

    add two edit methods, one for get, one for post

--- Lab2_Car/Controllers/CarController.cs
+++ Lab2_Car/Controllers/CarController.cs
@@ -1,16 +1,38 @@
 ﻿using Lab2_Car.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace Lab2_Car.Controllers
 {
     public class CarController : Controller
     {
-       
+        static Car Auto = new Car() { Make="Nissan", Model="March", Type=EngineType.Petrol, EngineSize="50CC"};
+
         public ActionResult Display()
         {
-            Car car = new Car() { Make="Nissan", Model="March", Type=EngineType.Petrol, EngineSize="50CC"};
-            return View(car);
+            return View(Auto);
+        }
+
+        [HttpGet]
+        public ActionResult Edit()
+        {
+            return View(Auto);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Car _car)
+        {
+            if (ModelState.IsValid)
+            {
+                Auto.Model = _car.Model;
+                Auto.Make = _car.Make;
+                return RedirectToAction("Display");
+            }
+            else
+            {
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
 
        
